9ff50bb85e6d4c93877890987dbd7a7f853436dc
Scott <56418482+WSB98@users.noreply.github.com>
2023-06-24 16:20:03 -0400

    moved the flask web files to the bert model. hooked up and working. run the test_mini_bert.py for th flask app

--- bert_model/test_mini_bert.py
+++ bert_model/test_mini_bert.py
@@ -8,6 +8,8 @@ import torch
 import transformers
 import re
 from transformers import BertTokenizer, BertForQuestionAnswering
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 
 # Load the pre-trained BERT model and tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -32,7 +34,7 @@ Bob Wyman is a retired partner at Latham & Watkins LLP, has a background in envi
 Mark Bernstein has been a driving force in sustainability, focusing on influencing consumer behavior in areas like energy efficiency and recycling.
 The founders of Earthshot are Bob Wyman and Mark Bernstein.
 Together with industry experts like Academy Award-winning visual effects artist Rob Legato and augmented reality specialists Magnopus, Wyman and Bernstein co-founded Earthshot. This organization aims to inspire individuals to take action on climate change by creating positive, educational, and engaging experiences. They believe that the current negative-focused climate change messaging is ineffective, and instead, they emphasize the power of presenting a positive vision of the future.
-Earthshot, a non-profit organization, utilizes games and immersive experiences as tools to motivate and educate people about climate change. They recognize the impact of games as a medium for engagement, combining entertainment, interactivity, and education. The organization's board of directors consists of experts from diverse industries such as entertainment, gaming, climate policy, sustainability, and technology.
+The organization's board of directors and team consists of experts from diverse industries such as entertainment, gaming, climate policy, sustainability, and technology.
 Earthshot is actively working on various initiatives, including the development of hand-held games, podcasts, technology showcases, public relations, and social networking campaigns. In the long term, they have plans to create museum-quality interactive and immersive experiences for venues like museums, county fairs, and schools. To support their work, Earthshot relies on funding from in-kind contributions, individual donations, and grants, while actively promoting racial and gender equity in their collaborations and initiatives.
 Earthshot has additional goals, including the development of cleantech platforms for existing game franchises, the creation of unique character and narrative intellectual property, and potential collaborations on a film centered around the multi-player game "Norah's Arc."
 Overall, Bob Wyman and Mark Bernstein play crucial roles in Earthshot's mission to inspire climate action through innovative gaming experiences
@@ -43,6 +45,13 @@ Earthshot, as a non-profit organization, is funded through a combination of in-k
 Earthshot is actively working on expanding its reach and fundraising efforts. It states that the organization is building a significantly larger mailing list in preparation for an upcoming individual fundraising campaign scheduled for later in the year. Furthermore, Earthshot is seeking grants from foundations and other organizations that show an interest in supporting its work.
 '''
 
+team_dataset = '''
+Mark Bernstein is the president and co-founder of Earthshot.
+Bob Wyman is the Co-founder and a Board Chair of Earthshot.
+The Earthshot team includes: Nikki Buffa, partner at Latham and Watkins; Manuel Grace, Associate General Counsel at Disney; Jamar Graham, Product Manager at PlayerWON; Lauren Graham, Founder of Velvet Frame; Bruce Garfield, Founder of Garfield Agency; Nikhil Jain, Co-Founder of Oben Inc. and serial entrepeneur; Rob Legato, Triple Academy award winning special effects expert; Ariella Lehrer, CEO of Legacy Games; Kenny Leu, Actor and Entrepeneur; Clayton Munnings, evironmental economist and consultant; Vickie Patton, evnironmental defense Fund's General Counsel; John Preston, Founder of TEM Capital; Jerry Prochazka, CEO at Ganymede Games; Mike Vandenbergh, Professor of Law and Director of Climate Change Research at Vanderbilt University; Sean Watson, Innovation Strategist; Belinda Smith Walker, Founding Board Member of New Villiage Girls Academy;
+'''
+
+'''
 while True:
     # ask question
     question = input('\n\nQuestion: ')
@@ -90,4 +99,78 @@ while True:
     answer = re.sub(r"##", "", answer)
 
     
-    print("Answer: ", answer)
+    print("Answer: ", answer)'''
+
+# return the response from the http request
+def get_chatbot_response(question):
+    dataset = ''
+    lowerQ = question.lower()
+    # FILTER THE QUESTION FOR KEYWORDS TO CHOOSE DATASET
+    if 'bob' in lowerQ or 'wyman' in lowerQ or 'mark' in lowerQ or 'bernstein' in lowerQ or 'board' in lowerQ:
+        dataset = bob_and_mark_dataset
+        print('\nbob and mark')
+    elif 'invest' in lowerQ or 'financ' in lowerQ or 'money' in lowerQ or 'capital' in lowerQ or 'equity' in lowerQ:
+        dataset = investing_dataset
+        print('\ninvesting')
+    elif 'game' in lowerQ or 'gaming' in lowerQ or 'green city' in lowerQ or 'electro ave' in lowerQ or 'develop' in lowerQ:
+        dataset = gaming_dataset
+        print('\ngaming')
+    elif 'team' in lowerQ or 'president' in lowerQ or 'co-' in lowerQ:
+        dataset = team_dataset
+        print('\nteam')
+    else:
+        dataset = default_dataset
+        print('\ndefault')
+    
+    # join lines in set
+    dataset = dataset.split('\n')
+    dataset = " ".join(dataset)
+
+    # Tokenize the question and text
+    inputs = tokenizer.encode_plus(question, dataset, add_special_tokens=True, return_tensors='pt')
+
+    # Perform inference
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # get start and end scores
+    start_scores = outputs.start_logits
+    end_scores = outputs.end_logits
+
+    # Get the most probable answer
+    start_index = torch.argmax(start_scores)
+    end_index = torch.argmax(end_scores)
+    all_tokens = tokenizer.convert_ids_to_tokens(inputs['input_ids'].squeeze())
+    
+    # format the tokens
+    answer = ' '.join(all_tokens[start_index: end_index + 1])
+    answer = re.sub(r"##", "", answer)
+    
+
+    return answer
+
+#flask request and response handling
+app = Flask(__name__, template_folder='templates')
+CORS(app) #enable Cross Origin Resource Sharing (CORS)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/process-request', methods=['GET','POST'])
+def process_request():
+    if(request.method == 'POST'):
+
+        data = request.json
+
+        #process input
+        chatbot_res = {'message' : get_chatbot_response(data)}
+
+        return jsonify(chatbot_res)
+    
+    return 'method not allowed', 405
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port="80")
